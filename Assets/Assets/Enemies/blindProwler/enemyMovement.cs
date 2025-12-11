3313dff7faf72a24a56466b4351b0f837ff594e1
gustaw <gustaw@gustaw.gustaw>
2025-12-11 21:04:35 +0100

    odstepy miedzy patrolami i przypisanie modelu do wroga i dodanie state

--- Assets/Assets/Enemies/blindProwler/enemyMovement.cs
+++ Assets/Assets/Enemies/blindProwler/enemyMovement.cs
@@ -3,12 +3,22 @@ using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour
 {
+    [Header("DEBUG MODE!")]
+    public bool debugMode;
+    [Header("Definitions")]
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
 
+    [Header("Agent Settings")]
+    public float speed;
+    private Vector3 velocity;
+
+    [Header("Actions' settings")]
     public Vector3 walkPoint;
     bool walkPointSet;
+    private float walkPointInterval;
+    private float nextPatrolTime;
     public float walkPointRange;
 
     public float attackInterval;
@@ -17,29 +27,63 @@ public class enemyMovement : MonoBehaviour
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
 
+    public enum State
+    {
+        None,
+        Idle,
+        Walk,
+        Attack
+    }
+    public State state;
+
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        walkPointInterval = Random.Range(2,10);
+        nextPatrolTime = Time.time+walkPointInterval;
     }
 
     private void Update()
     {
+        velocity = agent.velocity;
+        StateController();
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
         if (!playerInSightRange && !playerInAttackRange) Patrol();
         if (playerInSightRange && !playerInAttackRange) Chase();
         if (playerInSightRange && playerInAttackRange) Attack();
+        Debug.Log(state);
+    }
+
+    void StateController()
+    {
+        state = State.None;
+        if(velocity.x !=0  || velocity.z!=0 ) //if adding run later - && velocity.x<=speed && velocity.z<=speed
+        {
+            state=State.Walk;
+        } else if (velocity.x ==0 && velocity.z ==0 && !alreadyAttacked)
+        {
+            state=State.Idle;
+        } else if (alreadyAttacked)
+        {
+            state=State.Attack;
+        }
     }
 
     private void Patrol()
     {
-        if (!walkPointSet) SearchWalkPoint();
-        if (walkPointSet) agent.SetDestination(walkPoint);
+        if (nextPatrolTime <= Time.time)
+        {
+            if (!walkPointSet) SearchWalkPoint();
+            if (walkPointSet) agent.SetDestination(walkPoint);
 
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+            if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
+            nextPatrolTime=Time.time+walkPointInterval;
+        }
 
     }
     private void SearchWalkPoint()
@@ -55,6 +99,22 @@ public class enemyMovement : MonoBehaviour
     }
     private void Attack()
     {
-        Debug.Log("Atak");
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            Debug.Log("Atak");
+            alreadyAttacked=true;
+            Invoke(nameof(AttackReset),attackInterval);
+        }
+        
     }
+
+    private void AttackReset()
+    {
+        alreadyAttacked=false;
+    }
+    
+    
 }
