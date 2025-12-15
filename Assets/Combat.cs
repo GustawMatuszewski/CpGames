9ebd5f63c358ef3961bb395838a0fb73a83d1c5a
GustawMatuszewski <gmatuszewski2008@gmail.com>
2025-12-15 08:12:27 +0000

    Core collision and damage system working

--- Assets/Combat.cs
+++ Assets/Combat.cs
@@ -3,43 +3,65 @@ using System.Collections.Generic;
 
 public class Combat : MonoBehaviour
 {
+    [Header("Debug Mode!!!!!!")]
+    public bool debugMode;
+
     [Header("References")]
     public List<Collider> ownerHitboxes = new List<Collider>();
-    public List<string> damageHitboxNameList = new List<string>();
+    public List<string> damageHitboxNameList = new List<string>(); //Zapytac Pawla czy woli jak to bedzie obiekt nie nazwa
     public string hitboxTag;
     AttackTemplate currentAttack;
-
+    public Collider currentCollision;
     public bool canAttack;
 
     void FixedUpdate() {
-        ApplyDamage();
+        if(canAttack)
+            ApplyDamage();
     }
     
     
     public void ApplyDamage(){
-        if(DamageCollider(HitboxDetector()) != null){
-            EntityStatus status = DetectEntityStatus();
+        currentCollision = HitboxDetector();
+        if(DamageCollider(currentCollision) != null){
+            EntityStatus status = DetectEntityStatus(currentCollision);
             status.ApplyDamage(20f);
-            Debug.Log(HitboxDetector());
         }
+        currentCollision = null;
     }
+    public Collider HitboxDetector(){
+        foreach (Collider col in ownerHitboxes){
+            BoxCollider box = col as BoxCollider;
+            if (box == null)
+                continue;
 
-public Collider HitboxDetector() {
-    foreach (Collider hitbox in ownerHitboxes) {
-        Collider[] hits = Physics.OverlapBox(hitbox.bounds.center, hitbox.bounds.extents, hitbox.transform.rotation);
-        foreach (Collider hit in hits) {
-            if (hit.transform.root == transform.root) continue;
-            if (hit.CompareTag(hitboxTag))
+            Vector3 center = box.transform.TransformPoint(box.center);
+            Vector3 halfExtents = Vector3.Scale(box.size * 0.5f, box.transform.lossyScale);
+            Quaternion rotation = box.transform.rotation;
+
+            Collider[] hits = Physics.OverlapBox(center, halfExtents, rotation);
+
+            foreach (Collider hit in hits){
+                if (hit == col)
+                    continue;
+                if (ownerHitboxes.Contains(hit))
+                    continue;
+                if (hit.transform.IsChildOf(transform))
+                    continue;
+                if (!hit.CompareTag(hitboxTag))
+                    continue;
+                if(debugMode)
+                    Debug.Log("Hit: " + hit.name);
                 return hit;
+            }
         }
+
+        return null;
     }
-    return null;
-}
 
 
 
-    public EntityStatus DetectEntityStatus(){
-        Collider hit = HitboxDetector();
+
+    public EntityStatus DetectEntityStatus(Collider hit){
         if (hit != null){
             EntityStatus status = hit.GetComponentInParent<EntityStatus>();
             if (status != null)
