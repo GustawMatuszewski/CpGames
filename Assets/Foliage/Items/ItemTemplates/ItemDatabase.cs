b15dc18bcc65960b29be3684d64bdbe19a1b7cb5
manfixed2600 <matthewmatusiak.2008@gmail.com>
2026-03-02 16:35:32 +0100

    Revert "Moved to a whole new hdrp project"
    This reverts commit e09366665a05a4fb37a0cf62a270738df26f0dcf.

--- Assets/Foliage/Items/ItemTemplates/ItemDatabase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Inventory/ItemDatabase")]
-public class ItemDatabase : ScriptableObject
-{
-    public List<Item> allItems = new List<Item>();
-}
