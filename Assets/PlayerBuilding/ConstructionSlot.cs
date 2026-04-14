b38146901f03b95b41c09d76fdbb8f02273c6479
GustawMatuszewski <gmatuszewski2008@gmail.com>
2026-04-14 17:31:57 +0200

    TEst wIP

--- Assets/PlayerBuilding/ConstructionSlot.cs
+++ Assets/PlayerBuilding/ConstructionSlot.cs
@@ -3,41 +3,55 @@ using System.Collections.Generic;
 
 public class ConstructionSlot : MonoBehaviour, IInteractable
 {
-    public VariantUpgradeManager manager;
     public bool isFilled = false;
-    
-    public GameObject filledVisual;
-    public GameObject unfilledVisual;
+
+    ConstructionGhost ghost;
+    Renderer indicator;
+    Material unfilledMat;
+    Material filledMat;
 
     public bool UseSnapping => false;
-    public List<Transform> InteractionPositions => null;
-    public Transform LookAtTarget => null;
+    public List<Transform> InteractionPositions => new List<Transform> { transform };
+    public Transform LookAtTarget => transform;
 
-    void Awake()
+    public void Init(ConstructionGhost ghost, Material unfilled, Material filled)
     {
-        if (manager == null)
-            manager = GetComponentInParent<VariantUpgradeManager>();
-            
-        UpdateVisuals();
+        this.ghost = ghost;
+        unfilledMat = unfilled;
+        filledMat = filled;
+
+        BoxCollider col = gameObject.AddComponent<BoxCollider>();
+        col.size = Vector3.one * 0.4f;
+        col.isTrigger = false;
+
+        // Visible indicator cube
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.SetParent(transform);
+        cube.transform.localPosition = Vector3.zero;
+        cube.transform.localScale = Vector3.one * 0.3f;
+        Destroy(cube.GetComponent<Collider>()); // collider is on parent
+
+        indicator = cube.GetComponent<Renderer>();
+        UpdateVisual();
     }
 
     public void OnInteract()
     {
         if (isFilled) return;
-
         Fill();
     }
 
     public void Fill()
     {
         isFilled = true;
-        UpdateVisuals();
-        manager?.CheckAllFilled();
+        UpdateVisual();
+        ghost?.CheckAllFilled();
     }
 
-    void UpdateVisuals()
+    void UpdateVisual()
     {
-        if (unfilledVisual != null) unfilledVisual.SetActive(!isFilled);
-        if (filledVisual != null) filledVisual.SetActive(isFilled);
+        if (indicator == null) return;
+        if (isFilled && filledMat != null) indicator.material = filledMat;
+        else if (!isFilled && unfilledMat != null) indicator.material = unfilledMat;
     }
 }
